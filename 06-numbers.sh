754948d25f2fd4eb6d865dea07e94d64e294c656
ramu2 <ramu221@gmail.comgit config --global user.name ramu2cleargit config --global user.email ramu221@gmail.com>
2023-12-28 21:17:57 +0530

    practicing git

---
+++ 06-numbers.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+NUMBER1=$1
+NUMBER2=$2
+
+SUM=$($NUMBER1+ $NUMBER2)
+
+echo "sum is $SUM"
+
+if [ $NUMBER1 -gt $NUMBER2 ]
+then
+    echo " $NUMBER1 is greaterthan $NUMBER2"
+else
+    echo " $NUMBER1 is less than $NUMBER2"
+
+fi
