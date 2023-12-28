b1513a817694c80db9cf10d6dfb88db897dc7e9a
ramu2 <ramu221@gmail.comgit config --global user.name ramu2cleargit config --global user.email ramu221@gmail.com>
2023-12-28 22:04:19 +0530

    practicing git

---
+++ 07-array.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+FRUITS=("apple" "banana" "grapes" "orange")
+
+echo " First fruit name : ${FRUITS[0]}"
+echo " second fruit name : ${FRUITS[1]}"
+echo " third fruit name : ${FRUITS[2]}"
+echo " Fourth fruit name : ${FRUITS[3]}"
+
+
+echo " All Fruits names are : ${FRUITS[@]}"
