ec493d8cb636cbc4aba063b2fd7099afb653a581
ramu2 <ramu221@gmail.comgit config --global user.name ramu2cleargit config --global user.email ramu221@gmail.com>
2023-12-28 22:39:42 +0530

    practicing git

---
+++ 10-logfile.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+TIMESTAMP=$(date+%F-%H-%M-%s)
+LOGFILE="/temp/$0-$TIMESTAMP.log"
+
+ID=$(id -u)
+
+if [ $ID -ne 0 ]
+then
+    echo : "ERROR : Please run this script with root user"
+    exit 1
+else
+    echo " You are root user"
+fi
+
+VALIDATE() {
+
+if [ $1 -ne 0 ]
+then
+    echo " $2 is failed"
+    exit 1
+else  
+    echo " $2 is success"
+fi
+}
+
+yum install mysql -y $>> $LOGFILE
+VALIDATE $? " installing mysql"
+
+yum install git -y $>> $LOGFILE
+VALIDATE $? " installing git"
