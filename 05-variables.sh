c163b2d212a01b241e4b5d02b188cccc2b0b4ea0
ramu2 <ramu221@gmail.comgit config --global user.name ramu2cleargit config --global user.email ramu221@gmail.com>
2023-12-28 21:05:35 +0530

    practicing git

--- 05-variables.sh
+++ 05-variables.sh
@@ -3,5 +3,11 @@
 USERNAME=$1
 PASSWD=$2
 
-echo " Username : $USERNAME "
-echo " Passwd : $PASSWD "
+#echo " Username : $USERNAME "
+#echo " Passwd : $PASSWD "
+
+echo " please enter your user name"
+read -s USERNAME
+
+echo " please enter passwd"
+read -s PASSWD
