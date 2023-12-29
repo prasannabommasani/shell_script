04e063d77c18ecef5a3d28827953b4eccb8b74ad
ramu2 <ramu221@gmail.comgit config --global user.name ramu2cleargit config --global user.email ramu221@gmail.com>
2023-12-29 16:24:29 +0530

    practicing git

---
+++ 11-colours.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+R="/e[31m"
+G="/e[32m"
+Y="/e[33m"
+N="/e[0m"
+
+echo -e "$R Hi Good Morning $N"
+echo -e "$G Hi Good afternoon$N"
+echo -e "$Y Hi Good Evening $N"
+echo -e "$R Hi Good night $N"
+
