d4d65d235ed382b9bad4f61265731433f0611b8d
ramu2 <ramu221@gmail.comgit config --global user.name ramu2cleargit config --global user.email ramu221@gmail.com>
2023-12-29 17:10:51 +0530

    practicing git

--- install-package-loops.sh
+++ install-package-loops.sh
@@ -10,7 +10,8 @@ N="\e[0m"
 TIMESTAMP=$(date +%F-%H-%M-%S)
 
 LOGFILE="/tmp/$0-$TIMESTAMP.log"
-echo "script started at $TIMESTAMP" &>> LOGFILE
+
+echo "script started at $TIMESTAMP" 
 
 if [ $ID -ne 0 ]
 then
